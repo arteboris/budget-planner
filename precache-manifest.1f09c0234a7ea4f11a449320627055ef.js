30d7216ff823b883059b0c8109ac2cff2a84c0ed
arteboris <arteboris@ukr.net>
2019-10-25 15:40:00 +0300

    Updates

---
+++ precache-manifest.1f09c0234a7ea4f11a449320627055ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6cd2f2210df3e184886fcbcfe49d4e",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "1756789ca5fa1081fbb6",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.9d29a876.chunk.css"
+  },
+  {
+    "revision": "a00ecd9401cbeb61da4d",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.e1822b3d.chunk.js"
+  },
+  {
+    "revision": "1756789ca5fa1081fbb6",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.4ea6ead4.chunk.js"
+  },
+  {
+    "revision": "dffe8cd1e2fc699743fb",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.d2e8316a.js"
+  }
+]);
